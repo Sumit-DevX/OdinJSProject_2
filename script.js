3a0c7316d908e8f46c4518faeb947f188d9b60e3
Sumit Nayak <sumit.debugged@gmail.com>
2025-12-16 20:38:19 +0530

    implement sketch logic

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-
+let isDragging = false;
 document.addEventListener("DOMContentLoaded", () => {
     let container = document.querySelector('.container');
-    let button = document.querySelector('.change');
+    
     const divList = {};
 
-    for(let i=0; i < 256; i++){
+    for(let i=0; i < 16384; i++){
         divList[i] = document.createElement('div');
         divList[i].className = "grid";
         container.appendChild(divList[i]);
+
+        
+        divList[i].addEventListener('mousedown', () => {
+            isDragging = true;
+        });
+
+        divList[i].addEventListener('mousemove', () => {
+            if(!isDragging) return ;
+            divList[i].classList.add("change-color");
+        });
+
+        divList[i].addEventListener("mouseup", () => {
+            isDragging = false;
+        })
     }
 
     
